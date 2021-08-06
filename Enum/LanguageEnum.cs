906ce08200adc0f64a487a11a227ace870369f85
Lokesh <lokeshdotg1@gmail.com>
2021-08-06 12:34:33 +0530

    Update text and data of the enum

--- Enum/LanguageEnum.cs
+++ Enum/LanguageEnum.cs
@@ -1,14 +1,18 @@
+
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore.Enum
 {
     public enum LanguageEnum
     {
-        Hindi,
-        English,
-        Urdu,
-        French,
-        Tamil,
-        Marathi,
-        BhojPuri,
-        Japanese,
+        [Display(Name="Hindi Language")] //To change text of existing language
+        Hindi=10,
+        English=20,
+        Urdu=30,
+        French=40,
+        Tamil=60,
+        Marathi=70,
+        BhojPuri=80,
+        Japanese=90,
     }
 }
