c4d5b4b08df7cac7e836082df71e12a453078c85
Lokesh <lokeshdotg1@gmail.com>
2021-08-16 12:15:30 +0530

    Override a Html Element using custom taghelper

---
+++ Helper/HtmlElementOverrideTagHelper.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookStore.Helper
+{
+    [HtmlTargetElement("big")]
+     [HtmlTargetElement(Attributes="big")]
+    public class HtmlElementOverrideTagHelper: TagHelper
+    {
+        public override void Process(TagHelperContext context,TagHelperOutput output)
+        {
+            output.TagName="h3";
+            output.Attributes.RemoveAll("big");
+            output.Attributes.SetAttribute("class","h3");
+        }
+    }
+}
